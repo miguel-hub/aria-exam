e329895a69b158947f3d867f7f5230590c8cfe6b
miguel-castillo <miguel_castillo@me.com>
2014-03-26 21:10:34 +0000

    Another Commit

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 	$('#intro').parallax("50%", 0.1);
 	$('#second').parallax("50%", 0.1);
 	$('#third').parallax("50%", 0.3);
+	$('#fourth').parallax("50%", 0.7);
 
 });
 
